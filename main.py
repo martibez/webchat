5cc4c3450b0dfc88a55c7f0f1bbdad08c38e1b6a
Bruno da Camara <bruno.dacamara@softtek.com>
2023-08-09 18:47:23 -0300

    indent code

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template # estruturas para criar o site
-from flask_socketio import SocketIO, send # estruturas para criar o chat
+from flask import Flask, render_template  # estrutura para criar o site
+from flask_socketio import SocketIO, send  # estrutura para criar o chat
 
-app = Flask(__name__) # cria o site
-app.config["SECRET"] = "ajuiahfa78fh9f78shfs768fgs7f6" # chave de seguranca, pode ser qualquer coisa, mas escolha algo dificil
-app.config["DEBUG"] = True # para testarmos o código, no final tiramos
-socketio = SocketIO(app, cors_allowed_origins="*") # cria a conexão entre diferentes máquinas que estão no mesmo site
+app = Flask(__name__)  # cria o site
+# chave de segurança, pode ser qualquer coisa, mas escolha algo difícil
+app.config["SECRET"] = "ajuiahfa78fh9f78shfs768fgs7f6"
+app.config["DEBUG"] = True  # para testarmos o código, no final tiramos
+# cria a conexão entre diferentes máquinas que estão no mesmo site
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on("message") # define que a função abaixo vai ser acionada quando o evento de "message" acontecer
+
+# define que a função abaixo vai ser acionada quando o evento de "message" acontecer
+@socketio.on("message")
 def gerenciar_mensagens(mensagem):
     print(f"Mensagem: {mensagem}")
-    send(mensagem, broadcast=True) # envia a mensagem para todo mundo conectado no site
+    # envia a mensagem para todo mundo conectado no site
+    send(mensagem, broadcast=True)
 
-@app.route("/") # cria a página do site
+
+@app.route("/")  # cria a página do site
 def home():
-    return render_template("index.html") # essa página vai carregar esse arquivo html que está aqui
+    # essa página vai carregar esse arquivo html que está aqui
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    socketio.run(app, host='192.168.5.156') # define que o app vai rodar no seu servidor local, ou seja, na internet em que o seu computador tá conectado
- 
+    # define que o app vai rodar no seu servidor local, ou seja, na internet em que o seu computador tá conectado
+    socketio.run(app, host='192.168.5.156')
